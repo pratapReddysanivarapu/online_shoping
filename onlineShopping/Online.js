0879008773d0a9d0737493248f3d0d1bd0a732b6
pratapReddysanivarapu <pratapreddy1422@gmail.com>
2024-09-17 09:37:24 +0530

    Add files via upload

---
+++ onlineShopping/Online.js
@@ -0,0 +1,117 @@
+var a=0,b=0;
+function p1(){
+a+=1350;
+b++
+var v=confirm(" the selected items are "+b+" The toal amount is "+a);
+if(v==true)
+{
+window.open("Address.html");
+console.log(" the selected items are "+b+" The toal amount is "+a);
+}
+}
+
+function p2(){
+a+=1050;
+b++
+var v=confirm(" the selected items are "+b+" The toal amount is "+a);
+if(v==true)
+{
+window.open("Address.html");
+}
+}
+function p3(){
+a+=850;
+b++
+var v=confirm(" the selected items are "+b+" The toal amount is "+a);
+if(v==true)
+{
+window.open("Address.html");
+}
+}
+function p4(){
+a+=1250;
+b++
+var v=confirm(" the selected items are "+b+" The toal amount is "+a);
+if(v==true)
+{
+window.open("Address.html");
+}
+}
+function p5(){
+a+=1850;
+b++
+var v=confirm(" the selected items are "+b+" The toal amount is "+a);
+if(v==true)
+{
+window.open("Address.html");
+}
+}
+function p6(){
+a+=1550;
+b++
+var v=confirm(" the selected items are "+b+" The toal amount is "+a);
+if(v==true)
+{
+window.open("Address.html");
+}
+}
+function p7(){
+    a+=1000;
+    b++
+    var v=confirm(" the selected items are "+b+" The toal amount is "+a);
+    if(v==true)
+    {
+    window.open("Address.html");
+    }
+    }
+    function p8(){
+        a+=650;
+        b++
+        var v=confirm(" the selected items are "+b+" The toal amount is "+a);
+        if(v==true)
+        {
+        window.open("Address.html");
+        }
+        }
+
+        function p9(){
+            a+=389;
+            b++
+            var v=confirm("the selected items are "+b+"the total amount is"+a);
+            if(v==true)
+            {
+                window.open("Address.html");
+            }
+        }
+
+        function p10(){
+            a+=1000;
+            b++
+            var v=confirm("the selected items are "+b+"the total amount is"+a);
+            if(v==true)
+            {
+                window.open("Address.html");
+            }
+        }
+
+        function p11(){
+            a+=890;
+            b++
+            var v=confirm("the selected items are "+b+"the total amount is"+a);
+            if(v==true)
+            {
+                window.open("Address.html");
+            }
+        }
+
+        function p12(){
+            a+=1200;
+            b++
+            var v=confirm("the selected items are "+b+"the total amount is"+a);
+            if(v==true)
+            {
+                window.open("Address.html");
+            }
+        }
+        
+    
